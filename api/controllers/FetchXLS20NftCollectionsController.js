5a5626a702f757245451f9999a2ea047e3f9089e
Tushar Pardhe <tpardhe@gmail.com>
2022-12-08 15:44:31 +0000

    Added XLS20 controller

---
+++ api/controllers/FetchXLS20NftCollectionsController.js
@@ -0,0 +1,58 @@
+const mongoClient = Richlist.getDatastore().manager.client;
+const { response } = require('./response');
+
+const fetchXls20NftsFromDB = async (req, res) => {
+    const resObj = {
+        success: false,
+        error: false,
+        message: '',
+        data: {},
+    };
+    try {
+        let data = await mongoClient.db('XRPL').collection('xls20Nfts').find().toArray();
+
+        if (!data) {
+            resObj.data = null;
+            resObj.success = false;
+            resObj.error = true;
+            resObj.message = 'Internal Error';
+            response(resObj, res);
+            return;
+        }
+
+        const { query } = req;
+        let { limit, page } = query;
+        limit = limit ? parseInt(limit) : 10;
+        page = page ? parseInt(page) - 1 : 0;
+
+        if (limit < 0 || page < 0 || limit > 200) {
+            resObj.data = null;
+            resObj.success = false;
+            resObj.error = true;
+            resObj.message = 'Bad Params';
+            response(resObj, res);
+            return;
+        }
+
+        // slicing based on limit and page number
+        const startingIndex = page * limit;
+        const endingIndex = limit + startingIndex;
+        data = data.slice(startingIndex, endingIndex);
+
+        resObj.data = data;
+        resObj.success = true;
+        resObj.error = false;
+        resObj.message = 'Success';
+        response(resObj, res);
+    } catch (error) {
+        resObj.success = false;
+        resObj.error = true;
+        resObj.message = 'Some error occured';
+        console.error(error);
+        response(resObj, res);
+    }
+};
+
+module.exports = {
+    fetch: fetchXls20NftsFromDB,
+};
