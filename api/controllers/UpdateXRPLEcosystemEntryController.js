bd9adb6857f3a81f6fa430db1ed65179c8930dc0
Tushar Pardhe <tpardhe@gmail.com>
2023-01-28 21:14:34 +0000

    Added editing feature in all routes

---
+++ api/controllers/UpdateXRPLEcosystemEntryController.js
@@ -0,0 +1,50 @@
+const mongoClient = Richlist.getDatastore().manager.client;
+const { response } = require('./response');
+
+const errorRes = (errorMsg, resObj, res) => {
+    resObj.data = null;
+    resObj.success = false;
+    resObj.error = true;
+    resObj.message = errorMsg;
+    response(resObj, res);
+};
+
+const updateEcosystemEntry = async (req, res) => {
+    const { body } = req;
+    const { id, data } = body;
+
+    const resObj = {
+        success: false,
+        error: false,
+        message: '',
+        data: {},
+    };
+
+    try {
+        if (!data || !id || data.length === 0) {
+            errorRes('Please check the data again', resObj, res);
+            return;
+        }
+
+        const ecosystem = await mongoClient.db('XRPL').collection('ecosystem');
+        const result = await ecosystem.updateOne({ _id: id }, { $set: { data } });
+
+        if (result.modifiedCount === 0) {
+            errorRes('No data found', resObj, res);
+            return;
+        }
+
+        resObj.data = null;
+        resObj.success = true;
+        resObj.error = false;
+        resObj.message = 'Success';
+        response(resObj, res);
+    } catch (error) {
+        errorRes('Some error occured', resObj, res);
+        console.log(error);
+    }
+};
+
+module.exports = {
+    save: updateEcosystemEntry,
+};
