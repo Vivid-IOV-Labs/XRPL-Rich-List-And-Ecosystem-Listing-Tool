135a8c242a41b661c15d9aaac000637098758b19
Tushar Pardhe <tpardhe@gmail.com>
2023-02-15 16:48:13 +0000

    Removed policy

--- config/policies.js
+++ config/policies.js
@@ -1,11 +1,1 @@
-module.exports.policies = {
-    SaveXRPLEcosystemEntryController: {
-        '*': ['isAdmin'],
-    },
-    SaveXRPLIousController: {
-        '*': ['isAdmin'],
-    },
-    SaveXRPLNftCollectionsController: {
-        '*': ['isAdmin'],
-    },
-};
+module.exports.policies = {};
